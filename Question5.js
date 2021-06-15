4c7a04e76f1902c7879307495bf20d4aac55c55e
Sujata Singh <42352629+Sujata-123@users.noreply.github.com>
2021-06-15 20:12:58 +0530

    Update Question5.js

--- Question5.js
+++ Question5.js
@@ -1,1 +1,10 @@
 // Write a function that computes the running total of a list.
+// Write a function that computes the running total of a list.
+let arr = [1, 2, 3, 4, 5];
+let result = [];
+arr.reduce(function(accumulator, currentValue) {
+  let totalvalue = accumulator + currentValue;
+  result.push(totalvalue);
+  return totalvalue;
+}, 0);
+console.log(result);
